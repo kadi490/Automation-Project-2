b654b6523c0d7c8e04d7b43ea5db51b6eb31de47
kadi490 <kadikangur@gmail.com>
2023-07-08 17:04:14 +0300

    Ticket creation, assigning, rando plugin

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -55,7 +57,7 @@ describe('Issue create', () => {
     });
   });
 
-  it.only('Task 1', () => {
+  it('Test 1 Creating a Bug by Pickle Rick', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       
@@ -72,11 +74,11 @@ describe('Issue create', () => {
       //Otherwise filling title first sometimes doesn't work due to web page implementation
       cy.get('input[name="title"]').type('Bug');
       
-      //Select Lord Gaben from reporter dropdown
+      //Select Pickle Rick from reporter dropdown
       cy.get('[data-testid="select:reporterId"]').click();
       cy.get('[data-testid="select-option:Pickle Rick"]').click();
       
-      //Select Lord Gaben from reporter dropdown
+      //Select the highest priority
       cy.get('[data-testid="select:priority"]').click();
       cy.get('[data-testid="select-option:Highest"]').click();
 
@@ -100,9 +102,61 @@ describe('Issue create', () => {
           .should('have.length', '5')
           .first()
           .find('p')
-          .contains('TEST_TITLE');
+          .contains('Bug');
       //Assert that correct avatar and type icon are visible
-      cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
+      cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
+      cy.get('[data-testid="icon:story"]').should('be.visible');
+    });
+  });
+
+  it('Test 2 Creating a Task by Baby Yoda', () => {
+    
+    const randomTitle = faker.animal.type();
+    const randomDescr = faker.company.buzzPhrase();
+
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+            
+      //Type description
+      cy.get('.ql-editor').type(randomDescr);
+
+      //Type title 
+      cy.get('input[name="title"]').type(randomTitle);
+      
+      //Select reporter as Baby Yoda
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+      //Assign the assignee as Baby Yoda to have avatar visible on board
+      cy.get('[data-testid="select:userIds"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+      
+      //Select priority
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').click();
+
+      //Click on "Create issue"
+      cy.get('button[type="submit"]').click();
+    });
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+    
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+          .should('have.length', '5')
+          .first()
+          .find('p')
+          .contains(randomTitle);
+
+      cy.get('[data-testid="avatar:Baby Yoda"]').should('be.visible');
       cy.get('[data-testid="icon:story"]').should('be.visible');
     });
   });
